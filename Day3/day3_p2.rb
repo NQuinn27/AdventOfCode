7f6524789b3a8ab3f1f0398ea5e1df755fb10bd8
Niall Quinn <niall@popdeem.com>
2015-12-03 11:58:42 +0000

    Day 3

--- Day3/day3_p2.rb
+++ Day3/day3_p2.rb
@@ -7,40 +7,20 @@ File.open('input.txt','r') do |f|
   hash["#{0},#{0}"] = "visited"
   switch = true
   f.each_char do |c| # iterate on each character
-     if c == '<' then
-       if switch then
-         santa_x -= 1
-       else
-         robo_x -= 1
-       end
-     end
-     if c == '>' then
-       if switch then
-         santa_x += 1
-       else
-         robo_x += 1
-       end
-     end
-     if c == '^' then
-       if switch then
-         santa_y += 1
-       else
-         robo_y += 1
-       end
-     end
-     if c == 'v' then
-       if switch then
-         santa_y -= 1
-       else
-         robo_y -= 1
-       end
-     end
-     if switch then
-       hash["#{santa_x},#{santa_y}"] = "visited"
-     else
-       hash["#{robo_x},#{robo_y}"] = "visited"
-     end
-     switch = !switch
+    if switch then
+      if c == '<' then santa_x -= 1 end
+      if c == '>' then santa_x += 1 end
+      if c == '^' then santa_y += 1 end
+      if c == 'v' then santa_y -= 1 end
+      hash["#{santa_x},#{santa_y}"] = "visited"
+    else
+      if c == '<' then robo_x -= 1 end
+      if c == '>' then robo_x += 1 end
+      if c == '^' then robo_y += 1 end
+      if c == 'v' then robo_y -= 1 end
+      hash["#{robo_x},#{robo_y}"] = "visited"
+    end
+    switch = !switch
   end
   puts "#{hash.length}"
 end
