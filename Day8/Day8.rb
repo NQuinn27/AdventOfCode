334e2e132606e30bd415ffb04d9ceebda8d21cf9
Niall Quinn <niall@popdeem.com>
2015-12-08 11:47:10 +0000

    Day7 was not my work - may revisit this.

--- Day8/Day8.rb
+++ Day8/Day8.rb
@@ -5,7 +5,7 @@ File.readlines("input.txt").each do |line|
   line = line.chomp
   code_chars += line.length
   memory_chars += eval(line).length
-  esc_chars += line.dump.length
+  encoded_length += line.dump.length
 end
 puts "Part-1:#{code_chars - memory_chars}"
-puts "Part-2:#{esc_chars - code_chars}"
+puts "Part-2:#{encoded_length - code_chars}"
