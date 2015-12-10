261bbe87469d793bac6aa3f1fc5eaafada92315c
Niall Quinn <niall@popdeem.com>
2015-12-10 13:48:23 +0000

    Day 10

---
+++ Day10/Day10.rb
@@ -0,0 +1,21 @@
+def look_say(n)
+  last_char = n[0]
+  count = 1;
+  new_output = ""
+  for i in 1..n.length
+    if n[i] == last_char
+      count += 1
+    else
+      new_output += "#{count}#{last_char}"
+      last_char = n[i]
+      count = 1
+    end
+  end
+  return new_output
+end
+input = "3113322113"
+for i in 1..50 do
+  puts "#{i}"
+  input = look_say(input)
+end
+puts input.length
